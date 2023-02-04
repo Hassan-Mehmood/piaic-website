aef623e6a3d2e8713ce745abf4317418b4a897f0
Hassan-Mehmood <hassanafridi1913@gmail.com>
2023-02-04 15:42:52 +0500

    Desktop nav complete Working on mobiel nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
+'use client';
+import Navbar from '@/components/Navbar';
+import { ChakraProvider } from '@chakra-ui/react';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body>
+        <ChakraProvider>
+          <Navbar />
+          {children}
+        </ChakraProvider>
+      </body>
     </html>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,60 @@
+import { Box, Button, Flex, ListItem, UnorderedList } from '@chakra-ui/react';
+import Link from 'next/link';
+import { MdOutlineMenu } from 'react-icons/md';
+
+export default function Navbar() {
+  return (
+    <Box as="header" bgColor={'#035554'}>
+      <Box as="nav" maxW="1440px" mx="auto" py="16px" px="16px">
+        <Flex alignItems="center" justifyContent="space-between">
+          <Box maxW="48px" cursor="pointer">
+            <img src="/panaverse80_80.png" alt="Panaverse Logo" />
+          </Box>
+
+          <Box display={{ base: 'none', lg: 'unset' }}>
+            <UnorderedList display="flex" alignItems="center" m="0px">
+              <ListItem listStyleType="none" mx="25px" color="#fff">
+                <Link href="">How It Works</Link>
+              </ListItem>
+              <ListItem listStyleType="none" mx="25px" color="#fff">
+                <Link href="">Apply</Link>
+              </ListItem>
+              <ListItem listStyleType="none" mx="25px" color="#fff">
+                <Link href="">Available Programs</Link>
+              </ListItem>
+              <ListItem listStyleType="none" mx="25px" color="#fff">
+                <Link href="">About</Link>
+              </ListItem>
+              <ListItem listStyleType="none" mx="25px" color="#fff">
+                <Link href="">WIT</Link>
+              </ListItem>
+            </UnorderedList>
+          </Box>
+          <Box display={{ base: 'none', lg: 'unset' }}>
+            <Button
+              color="#000"
+              px="25px"
+              py="5px"
+              bgImage="linear-gradient(270deg,#08d88d,#0eb5a3);"
+              _hover={{
+                backgroundImage: 'linear-gradient(270deg,#0eb5a3,#08d88d);',
+              }}
+              _focus={{
+                backgroundImage: 'linear-gradient(270deg,#0eb5a3,#08d88d);',
+              }}
+            >
+              Apply
+            </Button>
+          </Box>
+          <Box display={{ base: 'unset', lg: 'none' }} color="#fff" cursor="pointer">
+            <MdOutlineMenu size={24} />
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
+  );
+}
+
+function phoneNav() {
+  return <div>he</div>;
+}
